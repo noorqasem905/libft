d4438b70a34c397eb8136c68a4f282a0fbe0b572
nooralden qasem <151868091+noorqasem905@users.noreply.github.com>
2025-03-13 23:01:57 +0300

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -86,7 +86,7 @@ char	*ft_read(int fd, char *buf)
 			return (NULL);
 		}
 		temp[bytread] = '\0';
-		buf = ft_strjoin_line(buf, temp);
+		buf = ft_strjoin(buf, temp);
 		free(temp);
 	}
 	return (buf);
