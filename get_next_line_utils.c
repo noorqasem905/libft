e5b7c988d03d53f33e1479859a942a1776492386
nooralden qasem <151868091+noorqasem905@users.noreply.github.com>
2025-03-13 23:02:20 +0300

    Update get_next_line_utils.c

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -12,35 +12,6 @@
 
 #include "get_next_line.h"
 
-char	*ft_strjoin_line(char *s1, char *s2)
-{
-	int		i;
-	int		j;
-	char	*st;
-
-	j = 0;
-	i = 0;
-	if (!s1)
-		s1 = ft_calloc(1, 1);
-	if (!s1 && !s2)
-		return (NULL);
-	st = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
-	if (st == NULL)
-		return (NULL);
-	while (s1[i] != '\0')
-	{
-		st[i] = s1[i];
-		i++;
-	}
-	while (s2[j] != '\0')
-	{
-		st[i + j] = s2[j];
-		j++;
-	}
-	st[i + j] = '\0';
-	return (free(s1), free(s2), st);
-}
-
 char	*ft_strchr_vline(char *str)
 {
 	int	i;
